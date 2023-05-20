72b4eb40842a59d26087a4b63fd23a0be3e9dcbd
meganath2013 <meganath2013@gmail.com>
2023-05-20 01:51:58 -0700

    withUsestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Navbar from './Navbar';
+import Home from './Home';
+import { useState } from 'react';
 
 function App() {
+  let i=0
+
+  const [number, setNumber]=useState(50)
+  const [color,setColor]=useState('red')
+
+  const handleClick =(name1) => {
+    
+    console.log("Hey you "+ name1 , i )
+    if((i%2)==0)
+    {
+    setColor('blue')
+    setNumber(55)
+    }
+    else
+    {
+      setColor('red')
+      setNumber(50)
+    }
+    i++
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <Navbar/>
+        <button onClick={()=>handleClick('mega' )}>Button1</button>
+        <h1>
+          APP COMPONENTS HERE
+          <p>color is {color} , number is {number}
+          </p>
+          <Home/>
+        </h1>
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+<div className="Home">
+    <h2>Home Mega Page</h2>
+</div>
+
+     );
+}
+ 
+export default Home;
